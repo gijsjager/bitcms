e03e4bc7f9a219dd6b9cea81f598254b8860c30f
Gijs Jager <gijs.jager@epassi.com>
2026-01-17 00:02:32 +0100

    Improve cache pages

--- src/Utilities/PageCache.php
+++ src/Utilities/PageCache.php
@@ -9,7 +9,22 @@ use Cake\Http\ServerRequest;
 abstract class PageCache
 {
     const string CACHE_DIR = 'tmp/cache/page_cache';
+    static bool $enabled = true;
 
+    /**
+     * Call this method if you want to disable the page cache for the current request
+     * @return void
+     */
+    public static function disable(): void
+    {
+        self::$enabled = false;
+    }
+
+    /**
+     * Generate a cached version of the current page
+     * @param EventInterface $event
+     * @return void
+     */
     public static function generateForView(EventInterface $event): void
     {
         /** @var Response $response */
@@ -18,31 +33,41 @@ abstract class PageCache
         /** @var ServerRequest $request */
         $request = $event->getSubject()->getRequest();
 
-        if (
-            !$request->is('get') ||
-            $response->getStatusCode() !== 200 ||
-            $response->getType() !== 'text/html' ||
-            str_contains($request->getUri()->getPath(), '/bitcms')
-        ) {
+        if (!self::willUseCache($request, $response)) {
             return;
         }
 
         self::checkCacheDirectory();
         $fileName = self::generateCacheFileName($request->getRequestTarget());
 
-
         $filePath = ROOT . DS . self::CACHE_DIR . DS . $fileName;
+
+        $body = (string)$response->getBody();
+        if (empty($body)) {
+            return;
+        }
+
+        // append a cached string to the body
         $cachedString = '<!-- Cached on ' . date('Y-m-d H:i:s') . ' - By the amazing and empowering Gijs Code -->';
-        file_put_contents($filePath, (string)$response->getBody() . $cachedString);
+        file_put_contents($filePath, $body . $cachedString);
     }
 
+    /**
+     * Get the cached version of the current page if available
+     * @param EventInterface $event
+     * @return string|null
+     */
     public static function getForView(EventInterface $event): ?string
     {
-        // check if there is a cached version of the current URL and return it
-        /** @var ServerRequest $request */
         $request = $event->getSubject()->getRequest();
-        $url = $request->getRequestTarget();
+        $response = $event->getSubject()->getResponse();
 
+        if (!self::willUseCache($request, $response)) {
+            return null;
+        }
+
+        // check if there is a cached version of the current URL and return it
+        $url = $request->getRequestTarget();
         $fileName = self::generateCacheFileName($url);
         $filePath = ROOT . DS . self::CACHE_DIR . DS . $fileName;
         if (file_exists($filePath)) {
@@ -52,6 +77,28 @@ abstract class PageCache
         return null;
     }
 
+    /**
+     * Clear cached pages that match the given needle
+     * @param string $needle
+     * @return void
+     */
+    public static function clear(string $needle): void
+    {
+        $cacheDir = ROOT . DS . self::CACHE_DIR;
+        if (is_dir($cacheDir)) {
+            $files = glob($cacheDir . DS . '*.html');
+            foreach ($files as $file) {
+                if (str_contains($file, md5(preg_replace('/[^a-zA-Z0-9-_]/', '_', $needle)))) {
+                    unlink($file);
+                }
+            }
+        }
+    }
+
+    /**
+     * Clear all cached pages
+     * @return void
+     */
     public static function clearAll(): void
     {
         $cacheDir = ROOT . DS . self::CACHE_DIR;
@@ -63,6 +110,10 @@ abstract class PageCache
         }
     }
 
+    /**
+     * Ensure the cache directory exists
+     * @return void
+     */
     protected static function checkCacheDirectory(): void
     {
         $cacheDir = ROOT . DS . self::CACHE_DIR;
@@ -71,6 +122,11 @@ abstract class PageCache
         }
     }
 
+    /**
+     * Generate a cache file name based on the URL and user agent
+     * @param string $url
+     * @return string
+     */
     protected static function generateCacheFileName(string $url): string
     {
         // convert the url to a file name safe string
@@ -100,6 +156,28 @@ abstract class PageCache
             $fileName .= '_desktop';
         }
 
-        return md5($fileName) . '.html';
+        return $fileName . '.html';
+    }
+
+    /**
+     * Determine if the cache should be used for the given request and response
+     * @param ServerRequest $request
+     * @param Response $response
+     * @return bool
+     */
+    protected static function willUseCache(ServerRequest $request, Response $response): bool
+    {
+        if (
+            !self::$enabled ||
+            $request->getQuery('_nocache', false) ||
+            !$request->is('get') ||
+            $response->getStatusCode() !== 200 ||
+            $response->getType() !== 'text/html' ||
+            str_contains($request->getUri()->getPath(), '/bitcms')
+        ) {
+            return false;
+        }
+
+        return true;
     }
 }
